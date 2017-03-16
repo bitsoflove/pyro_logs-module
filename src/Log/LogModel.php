bc52ba9a655fe601fe284247f0a530ade1fbfbc3
Pepijn Olivier <olivier.pepijn@gmail.com>
2017-03-16 14:02:39 +0100

    Update LogModel.php

--- src/Log/LogModel.php
+++ src/Log/LogModel.php
@@ -1,8 +1,10 @@
 <?php namespace Bitsoflove\LogsModule\Log;
 
 use Anomaly\UsersModule\User\UserModel;
+use Bitsoflove\LogsModule\Exceptions\InvalidLogDataException;
 use Bitsoflove\LogsModule\Log\Contract\LogInterface;
 use Anomaly\Streams\Platform\Model\Logs\LogsLogsEntryModel;
+use Illuminate\Support\Facades\Log;
 
 class LogModel extends LogsLogsEntryModel implements LogInterface
 {
@@ -17,5 +19,41 @@ class LogModel extends LogsLogsEntryModel implements LogInterface
 
     public function user() {
         return $this->belongsTo(UserModel::class, 'user_id', 'id');
+    }
+
+    public function getDataAttribute() {
+        return json_decode($this->attributes['data'], true);
+    }
+
+    public function setDataAttribute($value) {
+
+        $jsonValue = $this->createJsonData($value);
+        $this->attributes['data'] = $jsonValue;
+    }
+
+    private function createJsonData($value) {
+
+        try {
+            // allow passing of an array. we will cast this to json here
+            if(is_array($value)) {
+                $value = json_encode($value, JSON_PRETTY_PRINT);
+            }
+
+            if(!$this->isJson($value)) {
+                throw new InvalidLogDataException("Cannot create JSON data for log entry - invalid input", $data);
+            }
+
+        } catch(\Exception $e) {
+            Log::error($e);
+            return null;
+        }
+
+        // by now, we're sure $value is a valid json string
+        return $value;
+    }
+
+    private function isJson($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
     }
 }
