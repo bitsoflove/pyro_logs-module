65ec0caa2ccbac0856005040cbb018f25ca8ee99
Pepijn Olivier <olivier.pepijn@gmail.com>
2017-08-28 10:29:04 +0200

    Update addon.php

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -3,7 +3,7 @@
 return [
     'title'       => 'Logs',
     'name'        => 'Logs Module',
-    'description' => '',
+    'description' => 'Generic and extensible database-based logs module for PyroCMS with CSV-export functionality',
     'section' => [
         'logs' => 'Logs',
     ]
